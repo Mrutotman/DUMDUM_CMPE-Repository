4b7ec0e15462aefe051cf458bd930a5be6943345
Mrutotman <itsmepula@gmail.com>
2025-10-10 13:14:32 +0800

    w

---
+++ Past codes lecture/Dumdum_Python Print and Input.py
@@ -0,0 +1,34 @@
+a = input("Pick a Number from 1-3: ")
+b = input("Pick a color:")
+match a:
+    case "1":
+        match b:
+        case "red":
+
+        case "blue":
+
+        case
+        "green":
+
+        case
+        "yellow":
+    case "2":
+        match b:
+            case "red":
+
+            case "blue":
+
+            case "green":
+
+            case "yellow":
+    case "3":
+        match b:
+            case "red":
+
+            case "blue":
+
+            case "green":
+
+            case "yellow":
+    case _:
+        print("Invalid choice")
