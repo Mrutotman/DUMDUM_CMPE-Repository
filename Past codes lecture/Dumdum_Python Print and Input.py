409c72cb51b5094cf4583c5f321fd9551054b49b
Mrutotman <itsmepula@gmail.com>
2025-10-11 12:30:12 +0800

    Added October 11, 2025 Python Code

--- Past codes lecture/Dumdum_Python Print and Input.py
+++ Past codes lecture/Dumdum_Python Print and Input.py
@@ -3,32 +3,33 @@ b = input("Pick a color:")
 match a:
     case "1":
         match b:
-        case "red":
-
-        case "blue":
-
-        case
-        "green":
-
-        case
-        "yellow":
+            case "red":
+                print("I am Andrei")
+            case "blue":
+                print("I am Padluck")
+            case "green":
+                print("I am RJ")
+            case "yellow":
+                print("I am Zild")
     case "2":
         match b:
             case "red":
-
+                print("I am Patrick")
             case "blue":
-
+                print("I am Skibidi")
             case "green":
-
+                print("I am so sigma")
             case "yellow":
+                print("I am Jim")
     case "3":
         match b:
             case "red":
-
+                print("I am Jomar")
             case "blue":
-
+                print("I am Sarah")
             case "green":
-
+                print("I am Hot")
             case "yellow":
+                print("I am Vince")
     case _:
         print("Invalid choice")
